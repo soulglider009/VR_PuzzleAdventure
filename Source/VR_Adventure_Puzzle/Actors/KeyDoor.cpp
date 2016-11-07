cd1ae4e6b4fe7e58faeec3ad9d185db90fcddebe
JASON-COMPUTER\Administrator <mrjasharp@gmail.com>
2016-11-08 00:34:03 +0800

    Added switch + door (requires activating in pre-determined order).

---
+++ Source/VR_Adventure_Puzzle/Actors/KeyDoor.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VR_Adventure_Puzzle.h"
+#include "KeyDoor.h"
+
+
+// Sets default values for this component's properties
+UKeyDoor::UKeyDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UKeyDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Register with all of the keys and listen for their toggle events
+	
+}
+
+
+void UKeyDoor::OnStatueUpdate(AActor* UpdatedStatue)
+{
+	//Go through each key value pair
+	//Then need to add a delegate to open door
+}
+
---
+++ Source/VR_Adventure_Puzzle/Actors/KeyDoor.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "KeyDoor.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class VR_ADVENTURE_PUZZLE_API UKeyDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UKeyDoor();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+public:
+
+};
